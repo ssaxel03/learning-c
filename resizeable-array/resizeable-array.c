e10cf969e8f5b7fe506f4e78c3c712e80e253c86
axel <ssaxel03@gmail.com>
2025-03-05 23:23:02 +0000

    feat: resizeable-array exercise

--- resizeable-array/resizeable-array.c
+++ resizeable-array/resizeable-array.c
@@ -1,48 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-struct log {
-    char move[50];
-    char desc[150];
-};
+typedef struct {
+    char **logs;    
+    size_t size;    
+    size_t capacity;
+} LogBook;
 
-int main() {
-
-    struct log logs[10];
-
-    struct log log1;
-    strcpy(log1.move, "WIT");
-    strcpy(log1.desc, "Widthdrawl of $ 100");
-    
-    struct log log2;
-    strcpy(log2.move, "WIT");
-    strcpy(log2.desc, "Widthdrawl of $ 200");
-    
-    struct log log3;
-    strcpy(log3.move, "WIT");
-    strcpy(log3.desc, "Widthdrawl of $ 300");
-    
-    struct log log4;
-    strcpy(log4.move, "WIT");
-    strcpy(log4.desc, "Widthdrawl of $ 400");
-
-    logs[0] = log1;
-    logs[1] = log2;
-    logs[2] = log3;
-    logs[3] = log4;
-
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%s - ", logs[i].move);
-        printf("%s\n", logs[i].desc);
-    }
-    
-
-    return 0;
+void initLogBook(LogBook *book, size_t capacity) {
+    book->logs = (char **)malloc(capacity * sizeof(char *));
+    book->size = 0;
+    book->capacity = capacity;
 }
 
-void addToArray(struct log logs[], struct log newLog) {
-    int i = 0;
-    
+void addLog(LogBook *book, const char *message) {
+    if (book->size == book->capacity) {
+        book->capacity *= 2;
+        book->logs = (char **)realloc(book->logs, book->capacity * sizeof(char *));
+    }
+    book->logs[book->size] = (char *)malloc(strlen(message) + 1); // +1 for '\0'
+    strcpy(book->logs[book->size], message);
+    book->size++;
+}
+
+void printLogs(LogBook *book) {
+    printf("Logs:\n");
+    for (size_t i = 0; i < book->size; i++) {
+        printf("%s\n", book->logs[i]);
+    }
+}
+
+void freeLogBook(LogBook *book) {
+    for (size_t i = 0; i < book->size; i++) {
+        free(book->logs[i]);
+    }
+    free(book->logs);
+    book->logs = NULL;
+    book->size = 0;
+    book->capacity = 0;
+}
+
+int main()
+{
+    LogBook logbook;
+    initLogBook(&logbook, 2);
+    int opt = 1;
+    char buffer[256];
+
+    while(opt != 3) {
+        printf("Captain's Log Menu\n");
+        printf("1 - Add log\n");
+        printf("2 - Print logs\n");
+        printf("3 - Exit\n");
+        scanf("%d", &opt);
+        getchar();
+
+        if(opt < 1 || opt > 3) {
+            printf("Option not available\n");
+            continue;
+        }
+
+        if(opt == 1) {
+            printf("Type the new log below:\n");
+            scanf("%255[^\n]%*c", buffer);
+
+            addLog(&logbook, buffer);
+            printf("Log added, Captain!\n");
+        }
+
+        if(opt == 2) {
+            printLogs(&logbook);
+        }
+    }
+
+    printf("Arr Captain, see you in another trip!\n");
+    freeLogBook(&logbook);
+
+    return 0;
 }
